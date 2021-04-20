c7834ff1dbdf9ecae318ea451de723c9d2565c74
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-04-20 08:12:19 -0400

    add canvas options

--- source/constants/options.js
+++ source/constants/options.js
@@ -1,1 +1,11 @@
 export const extensionNames = ['ANGLE_instanced_arrays'];
+
+export const canvasOptions = {
+    alpha: false,
+    depth: true,
+    stencil: false,
+    antialias: false,
+    desynchronized: true,
+    premultipliedAlpha: true,
+    preserveDrawingBuffer: false
+};
