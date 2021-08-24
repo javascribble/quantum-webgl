81d658fe211c36eaef2b8ee20e3b82c855c3452e
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-08-24 08:35:10 -0400

    remove canvas extensions

--- test/debug.js
+++ test/debug.js
@@ -1,6 +1,5 @@
 import '/node_modules/@javascribble/quantum/bundles/main-window.js';
 import '/node_modules/@javascribble/quantum-canvas/bundles/main.js';
-import '/node_modules/@javascribble/quantum-canvas/bundles/main-extensions.js';
 import '/node_modules/@javascribble/quantum-canvas/bundles/main-plugins.js';
 import '/bundles/main.js';
 import '/bundles/main-extensions.js';
